46482136558261532e9ddac81d47d690f2011ac1
Bart <kmiecikjob@gmail.com>
2023-07-18 18:33:29 +0200

    Installing xhr2 modules to send request through node

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-console.log('Hello World');
-
-
-// const xhr = new XMLHttpRequest();
-// url = "http://192.168.43.44:7860"
-// xhr.open("GET", "/");
-// xhr.send();
-// xhr.responseType = "json";
-// xhr.onload = () => {
-//   if (xhr.readyState == 4 && xhr.status == 200) {
-//     console.log(xhr.response);
-//   } else {
-//     console.log(`Error: ${xhr.status}`);
-//   }
-// };
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
+url = "http://192.168.43.44:7860"
+xhr.open("GET", url);
+xhr.send();
+console.log('Smt');
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    console.log(xhr.response);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
