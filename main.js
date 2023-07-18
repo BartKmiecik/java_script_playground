80e0d794147fa20baa9656cd43cb6bcba79643f5
Bart <kmiecikjob@gmail.com>
2023-07-18 18:28:55 +0200

    Hello world in js

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+
+// const xhr = new XMLHttpRequest();
+// url = "http://192.168.43.44:7860"
+// xhr.open("GET", "/");
+// xhr.send();
+// xhr.responseType = "json";
+// xhr.onload = () => {
+//   if (xhr.readyState == 4 && xhr.status == 200) {
+//     console.log(xhr.response);
+//   } else {
+//     console.log(`Error: ${xhr.status}`);
+//   }
+// };
